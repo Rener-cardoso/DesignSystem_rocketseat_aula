6b3fb2419f2c99d8562621d36f34d1dc28fd858f
Rener-cardoso <rener_cardoso@hotmail.com>
2024-04-18 23:22:37 -0300

    fix: Add subpath toStorybook on production

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -28,5 +28,12 @@ const config = {
   docs: {
     autodocs: "tag",
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/DesignSystem_rocketseat_aula/'
+    }
+
+    return config
+  }
 };
 export default config;
