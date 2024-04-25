186e8e67e69902f5fd0c1137c8a90bacb33395c5
Rener-cardoso <rener_cardoso@hotmail.com>
2024-04-25 15:26:32 -0300

    update design

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -7,5 +7,3 @@ export * from "./components/TextInput"
 export * from "./components/TextArea"
 export * from "./components/Checkbox"
 export * from "./components/MultiStep"
-
-export * from "./styles"
